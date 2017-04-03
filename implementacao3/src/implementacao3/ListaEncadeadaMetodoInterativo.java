95e2c8c6715a29faebc046c1cd785d0e54a5209b
Thiago Santiago <t_vps20@gmail.com>
2017-04-02 22:43:59 -0300

    Metodos implementados

--- implementacao3/src/implementacao3/ListaEncadeadaMetodoInterativo.java
+++ implementacao3/src/implementacao3/ListaEncadeadaMetodoInterativo.java
@@ -15,67 +15,72 @@ public class ListaEncadeadaMetodoInterativo implements ListaEncadeada_IF
     @Override
     public int size() {
         int tamanho = 0;
-        No aux = cabeca;
-        while(aux.getProximo() != null)
+        No atual = cabeca;
+        while(atual.getProximo() != null)
         {
             tamanho++;
-            aux = aux.getProximo();
+            atual = atual.getProximo();
         }
         return tamanho;
     }
 
     @Override
     public int search(int element) throws Exception {
-        return 0;
+        No atual = cabeca;
+        while((atual.getProximo() != null) && (atual.getDado() != element))
+            atual = atual.getProximo();
+        
+        if(atual.getDado() == element)
+            return atual.getDado();
+        else
+            throw new Exception("Elemento não encontrado");
+            
     }
 
     @Override
-    public void insert(int element) {
-        No auxCabeca = cabeca;
+    public void insert(int element) {       
         if(cabeca.getProximo() == null)
         {
             No novaCabeca = new No(element);
-            novaCabeca.setProximo(cabeca);
-            cabeca = novaCabeca;           
+            cabeca.setProximo(novaCabeca);
         }
         else
         {
-            while(auxCabeca.getProximo() != null)            
-                auxCabeca = auxCabeca.getProximo();
+            No atual = cabeca.getProximo();
             No novoNo = new No(element);
-            novoNo.setProximo(auxCabeca.getProximo());
-            auxCabeca.setProximo(novoNo);           
+            novoNo.setProximo(atual);
+            cabeca.setProximo(novoNo);
         }
     }
 
     @Override
     public void remove(int element) {
-       if(cabeca.getDado() == element)
-           cabeca = cabeca.getProximo();
-       else
-       {
-           No aux = cabeca;
-           //while ((aux != null) and (aux.getDado() != element))
+           No atual = cabeca;
+           No anterior = null;
+           while((atual.getProximo() != null) && (atual.getDado() != element))
            {
-               
+               anterior = atual;
+               atual = atual.getProximo();
            }
-       }
-       
+           
+           if(atual.getProximo() != null)
+               anterior.setProximo(atual.getProximo());
+           else
+               anterior.setProximo(null); 
     }
 
     @Override
     public int[] toArray() {
         int[] resultado = new int[size()];
-        No aux = cabeca;
+        No atual = cabeca;
         int contador = 0;
         
-        while(aux.getProximo() != null)
+        while(atual.getProximo() != null)
         {
-            resultado[contador] = aux.getDado();
-            aux = aux.getProximo();
+            atual = atual.getProximo();
+            resultado[contador] = atual.getDado();            
             contador++;
-        }
-        
+        }        
         return resultado;
     }    
 }
