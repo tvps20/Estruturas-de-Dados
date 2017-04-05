e0a668fe98fca5404244cea1e83d09c6c1905cd1
Thiago Santiago <t_vps20@gmail.com>
2017-04-05 11:39:24 -0300

    Criando o no da arvore

---
+++ implementacao3/src/implementacao3/NoArvore.java
@@ -0,0 +1,42 @@
+package implementacao3;
+
+public class NoArvore 
+{
+    private int dado;
+    private No esquerdo;
+    private No direito;
+    private No parente;
+
+    public int getDado() {
+        return dado;
+    }
+
+    public void setDado(int dado) {
+        this.dado = dado;
+    }
+
+    public No getEsquerdo() {
+        return esquerdo;
+    }
+
+    public void setEsquerdo(No esquerdo) {
+        this.esquerdo = esquerdo;
+    }
+
+    public No getDireito() {
+        return direito;
+    }
+
+    public void setDireito(No direito) {
+        this.direito = direito;
+    }
+
+    public No getParente() {
+        return parente;
+    }
+
+    public void setParente(No parente) {
+        this.parente = parente;
+    }
+    
+}
